7ed8a24660702342a1e7cff300e7b25322aa7289
farihatabassumeshita <fariha.eshita@gmail.com>
2026-01-08 14:10:47 +0600

    Class based serializer with Post

--- learning_project/core/employee/views.py
+++ learning_project/core/employee/views.py
@@ -12,3 +12,10 @@ class Employees(APIView):
         employee = Employee.objects.all()
         serializer = EmployeeSerializer(employee, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = EmployeeSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
