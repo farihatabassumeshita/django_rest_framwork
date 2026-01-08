565127f0eac94193f5dc1a66be723251b9fa0043
farihatabassumeshita <fariha.eshita@gmail.com>
2026-01-08 17:41:44 +0600

    Class based serializer with Primary Key

--- learning_project/core/api/urls.py
+++ learning_project/core/api/urls.py
@@ -1,5 +1,5 @@
 from home.views import index, person_list, student_view, student_details
-from employee.views import Employees
+from employee.views import Employees, EmployeeDetails
 from django.contrib import admin
 from django.urls import path
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('students/', student_view),
     path('students/<int:pk>/', student_details),
 
-    path('employee/', Employees.as_view())
+    path('employee/', Employees.as_view()),
+    path('employee/<int:pk>/', EmployeeDetails.as_view()),
+
 ]
--- learning_project/core/employee/views.py
+++ learning_project/core/employee/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.http import Http404
 from .models import Employee
 from .serializers import EmployeeSerializer
 
@@ -19,3 +20,16 @@ class Employees(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EmployeeDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
