88f9a955d53d3a1b190d94135cffd1854297430c
farihatabassumeshita <fariha.eshita@gmail.com>
2026-01-17 14:07:56 +0600

    Range Filter(when emp_id is integer)

--- learning_project/core/employee/filters.py
+++ learning_project/core/employee/filters.py
@@ -3,7 +3,9 @@ from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
     emp_designation = django_filters.CharFilter(field_name='emp_designation', lookup_expr='iexact')
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    emp_id = django_filters.RangeFilter(field_name='emp_id')
 
     class Meta:
         model = Employee
-        fields = ['emp_designation']
+        fields = '__all__'
